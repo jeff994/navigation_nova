f01ab8da9eccf1134919274eb6d3842724c88278
jeff994 <hujianbo1981@gmai.com>
2016-12-20 11:06:18 +0800

    issues on the function

--- scripts/robotjob.py
+++ scripts/robotjob.py
@@ -6,11 +6,11 @@ def clear_jobs(job_des, job_num):
 	del job_des[:]
 	del job_num[:]
 
-def generate_move(job_des, job_num, distance, direction)
+def generate_move(job_des, job_num, distance, direction):
 	job_num.extend([distance]) 
 	job_des.extend([direction])
 
-def generate_turn(job_des, job_num, angle)
+def generate_turn(job_des, job_num, angle):
 	job_num.extend([angle]) 
 	job_des.extend(['T'])
 
