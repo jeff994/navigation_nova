3461fcc4e5a8d7bed0f48acbd6adfae322a8b5f8
nova <hujianbo1981@gmail.com>
2016-12-25 17:39:14 +0800

    correct angle radians probems

--- scripts/robot_correction.py
+++ scripts/robot_correction.py
@@ -57,18 +57,18 @@ def update_robot_gps(left_encode, right_encode):
 		# a little bot of right turning 
 		if(left_dist > right_dist):	
 			R = total_dist * robot_drive.turn_radius / (left_dist - right_dist)
-			bearing = initial_bearing + 2 * half_alpha
+			bearing = initial_bearing - 2 * half_alpha
 		elif(right_dist > left_dist): 
 			R = total_dist * robot_drive.turn_radius / (right_dist - left_dist)
-			bearing = initial_bearing - 2 * half_alpha
+			bearing = initial_bearing + 2 * half_alpha
 	# scenario 04 robot moving backward
 	elif(left_dist < 0 and right_dist < 0):
 		if(left_dist < right_dist):
 			R = total_dist * robot_drive.turn_radius / (right_dist - left_dist)
-			bearing  = initial_bearing + 180 - 2 * half_alpha
+			bearing  = initial_bearing + 180 + 2 * half_alpha
 		elif(right_dist < left_dist):
 			R = total_dist * robot_drive.turn_radius / (left_dist - right_dist)
-			bearing  = initial_bearing + 180 + 2 * half_alpha
+			bearing  = initial_bearing + 180 - 2 * half_alpha
 	#for robot two wheels not moving at the same direction or once of the thing not moving 
 	# forwaring with rotation 
 	else:
@@ -76,16 +76,17 @@ def update_robot_gps(left_encode, right_encode):
 		r2 = abs(right_dist) / alpha 
 		R = abs(r1 - r2) 
 		if(left_dist >0 or (left_dist == 0 and  right_dist <0)): 
-			bearing = initial_bearing + 2 * half_alpha
-		else:
 			bearing = initial_bearing - 2 * half_alpha
+		else:
+			bearing = initial_bearing + 2 * half_alpha
 	
 	bearing = gpsmath.format_bearing(bearing)
-	distance = R * sin (half_alpha) * 2
-	rospy.loginfo("Distance moved %f, bearing %f, R %f", distance, half_alpha, R) 
+	dist =2 * R * sin (alpha/2.0)
+	rospy.loginfo("Distance moved %f, step_angle %f, R %f", dist, 2 * half_alpha, R) 
 	new_gps = gpsmath.get_gps(lon1, lat1, bearing, distance)		
 	robot_drive.bearing_now = bearing
 	robot_drive.lon_now = new_gps[0]
 	robot_drive.lat_now = new_gps[1]
 	
 	
+
