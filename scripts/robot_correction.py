bff2eb967ffc6aeae92aee960f4e2db0f8b5bb82
jeff994 <hujianbo1981@gmai.com>
2016-12-27 23:39:13 +0800

    Add some format and the double check the gps update calculation

--- scripts/robot_correction.py
+++ scripts/robot_correction.py
@@ -21,63 +21,66 @@ def update_robot_gps(left_encode, right_encode):
 		#no updating of information 
 		return
 
-	left_dist = left_encode / robot_drive.encode_to_mm
-	right_dist = right_encode / robot_drive.encode_to_mm
+	# loacal vaiables 
+	left_dist 	= left_encode / robot_drive.encode_to_mm
+	right_dist 	= right_encode / robot_drive.encode_to_mm
+	lon1 		= robot_drive.lon_now
+	lat1 		= robot_drive.lat_now
+	alpha 		= 0.0
+	total_dist 	= abs(left_dist) + abs(right_dist) 
+	bearing 	= 0.0
+	distance 	= 0.0 
+	R 			= 0.0; 
 
-	lon1 = robot_drive.lon_now
-	lat1 =  robot_drive.lat_now
-	initial_bearing = robot_drive.bearing_now	
-	
-	#rospy.loginfo("Bearing now %f,lon_now %f, lat_now %f", robot_drive.bearing_now, robot_drive.lon_now, robot_drive.lat_now)
-	# scenario 02 robot moving perfectly straight, bearing won't change, while lan and lon need to be updated 
+	# global vaiables 
+	robot_drive.step_angle 	= 0.0
+	rotot_drive.step_dist 	= (left_dist + right_dist) / 2
+
+	rospy.loginfo("Bearing now %f,lon_now %f, lat_now %f", robot_drive.bearing_now, robot_drive.lon_now, robot_drive.lat_now)
+	# scenario 01, 02 robot moving perfectly straight, bearing won't change, while lan and lon need to be updated 
 	if(left_dist == right_dist):
 		if(right_dist > 0 ):
-			robot_drive.lon_now, robot_drive.lat_now = gpsmath.get_gps(lon1, lat1, initial_bearing, left_dist)
+			robot_drive.lon_now, robot_drive.lat_now = gpsmath.get_gps(lon1, lat1, robot_drive.bearing_now, right_dist)
 		if(right_dist < 0):
-			robot_drive.lon_now, robot_drive.lat_now = gpsmath.get_gps(lon1, lat1, -initial_bearing, left_dist)
-		
+			robot_drive.lon_now, robot_drive.lat_now = gpsmath.get_gps(lon1, lat1, -robot_drive.bearing_now, right_dist)
 		#rospy.loginfo("Bearing now %f,lon_now %f, lat_now %f", robot_drive.bearing_now, robot_drive.lon_now, robot_drive.lat_now)
 		stringToSend = '%f %f %f' % (robot_drive.lon_now, robot_drive.lat_now, robot_drive.bearing_now)
 		robot_drive.pub_gps.publish(stringToSend)
 		return	
-
-	alpha = 0.0
-	total_dist = abs(left_dist) + abs(right_dist)
-	#convert to degree 
-	
-	bearing = 0.0
-	distance = 0.0 
-	R = 0.0; 
-	# scenario 03 robot moving forward 
+	# scenario 02 robot moving forward with slight 
 	# robot not so perfectly walking forward, eigher left wheel is faster or right wheel is faster 
-	if(left_dist > 0 and right_dist > 0):
-		# a little bot of right turning
-		alpha = (left_dist - right_dist)/(2.0 * robot_drive.turn_radius) 
-		R = total_dist * robot_drive.turn_radius / (left_dist - right_dist)
-		bearing = initial_bearing + degrees(alpha)/2
+	elif(left_dist > 0 and right_dist > 0):
+		# a little bit of right turning
+		alpha 	= (left_dist - right_dist) / (2.0 * robot_drive.turn_radius) 
+		R 		= total_dist * robot_drive.turn_radius / (left_dist - right_dist)
 	# scenario 04 robot moving backward
 	elif(left_dist < 0 and right_dist < 0):
-		alpha = (left_dist - right_dist)/(2.0 * robot_drive.turn_radius) 
-		R = total_dist * robot_drive.turn_radius / (right_dist - left_dist)
-		bearing  = initial_bearing + degrees(alpha)/2
-	#for robot two wheels not moving at the same direction or once of the thing not moving 
+		alpha 	= (left_dist - right_dist) / (2.0 * robot_drive.turn_radius) 
+		R 		= total_dist * robot_drive.turn_radius / (right_dist - left_dist)
+	# for robot two wheels not moving at the same direction or once of the thing not moving 
 	# forwaring with rotation 
 	else:
-		alpha = (abs(left_dist) + abs(right_dist)) / (2.0 * robot_drive.turn_radius)
-		r1 = abs(left_dist) / alpha
-		r2 = abs(right_dist) / alpha 
-		R = abs(r1 - r2) 
-		if(left_dist >0 or (left_dist == 0 and  right_dist <0)): 
-			bearing = initial_bearing + degrees(alpha)/2
+		alpha 	= total_dist / (2.0 * robot_drive.turn_radius)
+		r1 		= abs(left_dist) / alpha
+		r2 		= abs(right_dist) / alpha 
+		R 		= abs(r1 - r2)
+
+		#right turn 
+		if(left_dist > 0 or (left_dist == 0 and right_dist < 0)): 
+			alpha = alpha 
 		else:
-			bearing = initial_bearing + degrees(alpha)/2
-	
-	bearing = gpsmath.format_bearing(bearing)
-	dist = R * sin(degrees(abs(alpha))/2) * 2
-	rospy.loginfo("Distance moved %f, step_angle %f, R %f", dist, degrees(abs(alpha)), R) 
-	robot_drive.lon_now, robot_drive.lat_now  = gpsmath.get_gps(lon1, lat1, bearing, distance)		
-	robot_drive.bearing_now = bearing
-	stringToSend = '%f %f %f' % (robot_drive.lon_now, robot_drive.lat_now, robot_drive.bearing_now) #might need to add \n behind the E
+			alpha = -alpha 
+
+	robot_drive.step_angle 	= degrees(alpha)
+	# covnert to degree
+	bearing 				= initial_bearing + robot_drive.step_angle / 2
+	# convert between [0 - 360)
+	bearing 				= gpsmath.format_bearing(bearing)
+	dist 					= R * sin(robot_drive.step_angle) * 2
+	rospy.loginfo("Distance moved %f, step_angle %f, R %f, step_distance %f", dist, robot_drive.step_angle, R) 
+	robot_drive.lon_now, robot_drive.lat_now 	= gpsmath.get_gps(lon1, lat1, bearing, distance)		
+	robot_drive.bearing_now 					= gpsmath.format_bearing(initial_bearing + robot_drive.step_angle)
+	stringToSend 								= '%f %f %f' % (robot_drive.lon_now, robot_drive.lat_now, robot_drive.bearing_now) #might need to add \n behind the E
 	robot_drive.pub_gps.publish(stringToSend)
 	rospy.loginfo("Bearing now %f,lon_now %f, lat_now %f", robot_drive.bearing_now, robot_drive.lon_now, robot_drive.lat_now)
 
--- scripts/robot_drive.py
+++ scripts/robot_drive.py
@@ -8,35 +8,38 @@ from std_msgs.msg import String
 #	Robot drive module									#
 #-------------------------------------------------------#
 
-encode_to_mm = 69.00 	# 1000 encoding signals = 1 mm travelled
-turn_radius = 378 		# radius when turning in mm (half distance between the middle point of two wheels) 
-desired_speed = 5		# Global robot moving spped, 3 - 5
-speed_now = 5			# Robot moving speed now
-robot_on_mission = 0	# set an indicator that robot's on a job right now 
-robot_enabled = 0 		# A switch to enable or disable robot from execuing any jobs 
+encode_to_mm 		= 69.00 	# 1000 encoding signals = 1 mm travelled
+turn_radius 		= 378 		# radius when turning in mm (half distance between the middle point of two wheels) 
+desired_speed 		= 5			# Global robot moving spped, 3 - 5
+speed_now 			= 5			# Robot moving speed now
+robot_on_mission 	= 0			# set an indicator that robot's on a job right now 
+robot_enabled 		= 0 		# A switch to enable or disable robot from execuing any jobs 
 
 
-initial_bearing = 0 	#set as north for now
+initial_bearing 	= 0 	#set as north for now
 ############################################################
 
-lon_now = 31.205718
-lat_now  = 121.628857
-bearing_now =  0
+lon_now 			= 31.205718
+lat_now  			= 121.628857
+bearing_now 		= 0
 
-lon_target = 0.0
-lat_target = 0.0
-bearing_target = 0 		#degrees
+lon_target 			= 0.0
+lat_target 			= 0.0
+bearing_target 		= 0 		#degrees
 
-pub_command = rospy.Publisher('command', String, queue_size=10)
-pub_gps	= rospy.Publisher('gps', String, queue_size=10)
+step_angle 			= 0
+step_distance		= 0
+
+pub_command 		= rospy.Publisher('command', 	String, queue_size=10)
+pub_gps				= rospy.Publisher('gps', 		String, queue_size=10)
 
 def init_gps():
-	lon_now = 31.205718
-	lat_now  = 121.628857
-	bearing_now =  0
-	lon_target = 0.0
-	lat_target = 0.0
-	bearing_target = 0              #degrees
+	lon_now 		= 31.205718
+	lat_now  		= 121.628857
+	bearing_now 	= 0
+	lon_target 		= 0.0
+	lat_target 		= 0.0
+	bearing_target 	= 0              #degrees
 
 
 # Helper function which can send commands to robot 
