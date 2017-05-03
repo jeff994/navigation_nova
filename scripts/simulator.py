40092738251e066b1d2350b9db16095dd895e6a6
hujianbo1981@gmail.com <hj!!119827>
2017-05-03 16:09:03 +0800

    Updated the simulator

--- scripts/simulator.py
+++ scripts/simulator.py
@@ -20,42 +20,46 @@ def executor_simulator(data):
 	global left_encode
 	global right_encode
 	command_str = str(data.data)
+	rospy.loginfo("recieved command %s|", command_str)
         if  (command_str == 'SF000006E\n'):
             left_encode = 1700
-            right_encode  = 1600
-    	elif  (command_str == 'SF000005E\n'):
+            right_encode  = 1700
+	elif (command_str == 'SL000006E\n'):
+		left_encode = -2400
+		right_encode = 2400
+    	elif (command_str == 'SF000005E\n'):
     		left_encode = 1444
-    		right_encode  = 1430
+    		right_encode  = 1444
     	elif(command_str == 'SF000004E\n'):
     		left_encode = 1108
-    		right_encode = 1105
+    		right_encode = 1108
     	elif(command_str == 'SF000003E\n'):
-    		left_encode = 879
+    		left_encode = 867
     		right_encode = 867
         elif(command_str == 'SB000006E\n'):
             left_encode = -2000
-            right_encode  = -2017
+            right_encode  = -2000
     	elif(command_str == 'SB000005E\n'):
     		left_encode = -1444
-    		right_encode  = -1430
+    		right_encode  = -1444
     	elif(command_str == 'SB000004E\n'):
     		left_encode = -1108
-    		right_encode = -1105
+    		right_encode = -1108
     	elif(command_str == 'SB000003E\n'):
-    		left_encode = -879
+    		left_encode = -867
     		right_encode = -867
         elif(command_str == 'SL000006E\n'):
             left_encode = -2000
-            right_encode  = 2017
+            right_encode  = 2000
     	elif(command_str == 'SL000005E\n'):
     		left_encode = -1444
-    		right_encode  = 1430
+    		right_encode  = 1444
     	elif(command_str == 'SL000004E\n'):
     		left_encode = -1108
-    		right_encode = 1105
+    		right_encode = 1108
     	elif(command_str == 'SL000003E\n'):
     		left_encode = -879
-    		right_encode = 867
+    		right_encode = 879
         elif(command_str == 'SR000006E\n'):
             left_encode = 2000
             right_encode  = -2017
@@ -67,15 +71,14 @@ def executor_simulator(data):
     		right_encode = -1105
     	elif(command_str == 'SR000003E\n'):
     		left_encode = 879
-    		right_encode = -867
+    		right_encode = -879
     	else:
     		left_encode = 0
     		right_encode = 0
     	rospy.loginfo("I heard %s: %d:%d",command_str, left_encode, right_encode)
 
 def encoder_simulator():
-	global encoder_pub
-	
+	global encoder_pub	
 	bytesToPublish = '%d %d' % (left_encode, right_encode)
 	if(left_encode != 0  or right_encode != 0):
 	 	rospy.loginfo(bytesToPublish)
