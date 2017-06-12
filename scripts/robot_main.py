d96d0fb3a9773e130abb3f18ec8201a58ad572b2
aaron <hujianbo1981@gmail.com>
2017-06-12 10:13:36 +0800

    testing

--- scripts/robot_main.py
+++ scripts/robot_main.py
@@ -1,57 +1,57 @@
 #!/usr/bin/env python
 import rospy
 import string
-import time 
+import time
 import os
 import robot_obstacle
 import robot_job
 import robot_correction
 import robot_publisher
-import robot_listener 
-import robot_drive 
+import robot_listener
+import robot_drive
 import robot_move
-import robot_turn 
+import robot_turn
 import robot_configure
 import robot_status
 import execute_command    #only here to init command buffer
 from std_msgs.msg import String
 from geometry_msgs.msg import Vector3
 
-# The main progream process the robot logic 
+# The main progream process the robot logic
 def main_commander():
 	# ----------------------------------------------------------------------------------------#
 	#  code to check and use the ecnoder data                     							  #
 	# ----------------------------------------------------------------------------------------#
-	# publish parameters to the web server 
+	# publish parameters to the web server
 	#robot_publisher.publish_parameters()
 
-	if robot_drive.burn_mode: 
+	if robot_drive.burn_mode:
 		if not robot_drive.robot_enabled:
 			rospy.loginfo("Robot is on burn mode")
 			time.sleep(0.1)
-			return 
+			return
 		else:
 			rospy.loginfo("Robot enabled, switch to normal mode")
-			robot_drive.enter_normal_mode() 
+			robot_drive.enter_normal_mode()
 	else:
 		robot_publisher.publish_parameters()
 
-	# Very important error handling: 
-	# If Not any new data comming, waiting for next data, 
-	# if waiting too long need to issue warning or error	 
+	# Very important error handling:
+	# If Not any new data comming, waiting for next data,
+	# if waiting too long need to issue warning or error
 	if(robot_listener.encoder_received == robot_listener.encoder_processed): 	#aaron comment
 		robot_listener.process_encoder_delay() 									#aaron comment
 		return 																	#aaron comment
-	
-	# It process all the encoder data received - regardless robot status etc .  
-	#Including dynamically update robot GPS etc 
+
+	# It process all the encoder data received - regardless robot status etc .
+	#Including dynamically update robot GPS etc
 	robot_listener.process_encoder_data() 										#aaron comment
 
 	# ----------------------------------------------------------------------------------------#
 	#  code to close robot when required	                    							  #
 	# ----------------------------------------------------------------------------------------#
-	# If robot not enabled, just need to disable the robot 
-	if not robot_drive.robot_enabled: 
+	# If robot not enabled, just need to disable the robot
+	if not robot_drive.robot_enabled:
 		rospy.loginfo("Robot is disabled")
 		robot_job.disable_robot()
 		return
@@ -59,36 +59,36 @@ def main_commander():
 	# ----------------------------------------------------------------------------------------#
 	#  Codes for obstacle avoidence handling                     							  #
 	# ----------------------------------------------------------------------------------------#
-	# The flat would be set by hardware, we cannot do anything but blankly calculate the gps coordinates  
+	# The flat would be set by hardware, we cannot do anything but blankly calculate the gps coordinates
 	if robot_obstacle.robot_on_obstacle:
-		rospy.loginfo("Robot on obstacle avoidence, please wait") 
-		return 
+		rospy.loginfo("Robot on obstacle avoidence, please wait")
+		return
 
-	# Robot obstancle avoidence is over, now resume to normal operation 
+	# Robot obstancle avoidence is over, now resume to normal operation
 	if robot_obstacle.robot_over_obstacle:
 		robot_obstacle.complete_obstacle_avoidance()
-		return 
+		return
 
-	# handle robot paused conidtions 
+	# handle robot paused conidtions
 	if robot_drive.robot_paused:
-		rospy.loginfo("Pause robot") 
-		robot_job.pause_robot(); 
-		return; 
+		rospy.loginfo("Pause robot")
+		robot_job.pause_robot();
+		return;
 
 	# ----------------------------------------------------------------------------------------#
 	#  Codes for robot normal jobs like walking and turning       							  #
 	# ----------------------------------------------------------------------------------------#
 	# Check whether if there's any job left for the robot
-	# If no jobs, make sure robot stopped moving, we cannot leave robot moving there 
+	# If no jobs, make sure robot stopped moving, we cannot leave robot moving there
 	if not robot_job.has_jobs_left():
 		robot_job.process_no_job()
 		return
 
 	job_completed = robot_job.process_job()
-	
+
 	# ----------------------------------------------------------------------------------------#
 	#  Robot's doing the initialization jobs, not normal jobs      							  #
-	# ----------------------------------------------------------------------------------------# 
+	# ----------------------------------------------------------------------------------------#
 	#if not robot_drive.robot_initialized:
 	#	robot_job.complete_init_compass(compass_data[compass_index])
 	#	return
@@ -102,18 +102,18 @@ def main_commander():
 		if no_correction_jobs == 0:
 			rospy.loginfo("Complete a normal job, check whether correction is needed")
 			robot_correction.dist_correction_normal()
-		elif no_correction_jobs == 1: # the job is last correction job 
+		elif no_correction_jobs == 1: # the job is last correction job
 			rospy.loginfo("Complete all correction jobs, check whether further correction is needed")
 			robot_correction.dist_correction_correction();
 
 def read_system_config():
-	# Read configure path 
+	# Read configure path
 	print("Read configuration file")
 	config_path = os.path.dirname(os.path.abspath(__file__)) + '/robot.cfg'
 	size_para 	= 23
 	ret 		= [None] * size_para
-	
-	# Now reading configurable parameters 
+
+	# Now reading configurable parameters
 	# [tuning parameters] related
 	ret[0], robot_drive.linear_encode_to_mm 			= robot_configure.read_config_float(config_path, 'tuning', 'linear_encode_to_mm')
 	ret[1], robot_drive.turning_encode_to_mm			= robot_configure.read_config_float(config_path, 'tuning', 'turning_encode_to_mm')
@@ -136,7 +136,7 @@ def read_system_config():
 	ret[14], robot_turn.turn_full_speed 				= robot_configure.read_config_float(config_path, 'turn', 'turn_full_speed')
 	ret[15], robot_turn.turn_lower_speed 				= robot_configure.read_config_float(config_path, 'turn', 'turn_lower_speed')
 	ret[16], robot_turn.turn_lowest_speed 				= robot_configure.read_config_float(config_path, 'turn', 'turn_lowest_speed')
-	
+
 	# [init]
 	ret[17], robot_drive.obstacle_mode 					= robot_configure.read_config_float(config_path, 'init', 'obstacle_mode')
 	ret[18], robot_drive.robot_enabled 					= robot_configure.read_config_float(config_path, 'init', 'robot_enabled')
@@ -144,7 +144,7 @@ def read_system_config():
 	ret[20], robot_job.init_lon 						= robot_configure.read_config_float(config_path, 'init', 'init_lon')
 	ret[21], robot_job.init_lat 						= robot_configure.read_config_float(config_path, 'init', 'init_lat')
 	ret[22], robot_job.init_bearing 					= robot_configure.read_config_float(config_path, 'init', 'init_bearing')
-	
+
 	robot_drive.lon_now  		= robot_job.init_lon
 	robot_drive.lat_now  		= robot_job.init_lat
 	robot_drive.lon_target  	= robot_job.init_lon
@@ -152,16 +152,16 @@ def read_system_config():
 	robot_drive.bearing_now 	= robot_job.init_bearing
 	robot_drive.bearing_target 	= robot_job.init_bearing
 
-	# check whether the reading is successful or not 
+	# check whether the reading is successful or not
 	for index in range(size_para):
-		if not ret[index]: 
+		if not ret[index]:
 			print("The ",index,"parameter is not correct")
-	
+
 	print("Finished read configure file")
 	return
 
 
-#subscribes to different topic 
+#subscribes to different topic
 def main_listener():
 	rospy.init_node('commander')
 	#rospy.Subscriber('compass', String, robot_listener.compass_callback) 			#aaron comment
@@ -169,7 +169,7 @@ def main_listener():
 	rospy.Subscriber('keyboard', String, robot_listener.keyboard_callback)
 	#rospy.Subscriber('rc_sensor_f', String, robot_listener.rc_sensor_f_callback)  #write the callbacks properly
 	#rospy.Subscriber('rc_sensor_b', String, robot_listener.rc_sensor_b_callback)  #once we can get the correct data formats
-	#ADD SUBSCRIBERS FOR GYRO DATA, MAKE CALLBACKS TO HANDLE THEM	
+	#ADD SUBSCRIBERS FOR GYRO DATA, MAKE CALLBACKS TO HANDLE THEM
 
 	#@yuqing_obstacledriverread
 	rospy.Subscriber('obstacle_status', String, robot_listener.obstacle_status_callback) #was previously driver_obstacle
@@ -179,7 +179,7 @@ def main_listener():
 	rospy.Subscriber('IMU', Vector3, robot_listener.IMU_callback) #not yet done
 	rospy.Subscriber('battery', String, robot_listener.battery_callback)
 	rospy.Subscriber('direction', String, robot_listener.direction_callback)
-	
+
 	while not rospy.is_shutdown():
 		main_commander()
 	rospy.spin()
